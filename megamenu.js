216e4179065939cc1ed5d4f1f5f9b858414609de
Nacelio Barbosa <naceliomb@hotmail.com>
2021-09-19 09:30:15 -0300

    test push

--- megamenu.js
+++ megamenu.js
@@ -1,15 +1,6 @@
 function controlMegamenuUI(){
-    const megaMenu = document.getElementById("megamenu-wrapper");
-    let category = document.querySelectorAll
-
-    const closeMenu = document.querySelectorAll(".close-button");
-    const menuItems = megamenuNav.querySelectorAll("li");
-
-
-    
-    menuItems.forEach((menuItems) => {
-        hoverElement(menuItems);
-    });
+    //BASIC CONTROLS: OPEN, CLOSE, QUIT, HOVER ETC
+    const megaMenu = document.querySelector("#megamenu-wrapper");
 
 
 
