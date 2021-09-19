c6e17a36f014c091173d653075bc99675620eb84
Nacelio Barbosa <naceliomb@hotmail.com>
2021-09-19 02:51:06 -0300

    created archive for controller async execution

---
+++ controllerMegamenu.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+    asyncExecute();
+};
+
+async function asyncExecute() {
+    try {
+        await createDynamicMegamenu();
+    } catch (e) {
+        console.error(e);
+        //possible function return error handler
+    } finally {
+        controllMegamenuUI();
+    }
+}
